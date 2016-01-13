061ccf7a2fd59103404ddb9f7bcc05bd86bfb681
Slavikg <slavikg-g@yandex.ru>
2016-01-13 23:52:43 +0200

    Correct v.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
 			flash[:error] = exception.message
 			redirect_to root_url
 		end
+	end
 	rescue_from ActiveRecord::RecordNotFound do |exception|
 		flash[:error] = exception.message
 		redirect_to root_url
