48249d86a677e88abd799c16700f3588cbf4fb16
Slavikg <slavikg-g@yandex.ru>
2016-01-13 23:48:47 +0200

    Correct v.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  include SessionsHelper
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+	include SessionsHelper
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
-  end
+	unless Rails.application.config.consider_all_requests_local
+		rescue_from ActionController::RoutingError do |exception|
+			flash[:error] = exception.message
+			redirect_to root_url
+		end
+	rescue_from ActiveRecord::RecordNotFound do |exception|
+		flash[:error] = exception.message
+		redirect_to root_url
+	end
+	rescue_from CanCan::AccessDenied do |exception|
+		redirect_to root_url, :alert => exception.message
+	end
 end
